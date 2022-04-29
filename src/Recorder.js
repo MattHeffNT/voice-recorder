df1b9d0777e4844e6bf7b8356e4c1a1b510937c1
MattHeffNT <heffa2004@gmail.com>
2022-04-29 12:43:09 +0930

    add formatters

--- src/Recorder.js
+++ src/Recorder.js
@@ -1,48 +1,53 @@
 import React from 'react';
 import { saveAs } from 'file-saver';
 import { Button } from 'react-bootstrap';
-import flashing from './flashing.css'
+import flashing from './flashing.css';
 
 function Recorder() {
-    
-    const MicRecorder = require('mic-recorder-to-mp3');
-    const FileSaver = require('file-saver');
+  const MicRecorder = require('mic-recorder-to-mp3');
+  const FileSaver = require('file-saver');
 
-    const recorder = new MicRecorder({
-        bitRate: 128
-    });    
+  const recorder = new MicRecorder({
+    bitRate: 128,
+  });
 
-    // start recording function
-    function record_start() {
-      // toggle colour flashing
-      document.querySelector('#rec').classList.add("flashing");
-      recorder.start().then(() => {
-      }).catch((e) => {
+  // start recording function
+  function record_start() {
+    // toggle colour flashing
+    document.querySelector('#rec').classList.add('flashing');
+    recorder
+      .start()
+      .then(() => {})
+      .catch((e) => {
         console.error(e);
       });
-    }
+  }
 
-    // stop recording function then save to client device
-    function record_stop () {
-      document.querySelector('#rec').classList.remove("flashing");
-      recorder
+  // stop recording function then save to client device
+  function record_stop() {
+    document.querySelector('#rec').classList.remove('flashing');
+    recorder
       .stop()
-      .getMp3().then(([buffer, blob]) => {
+      .getMp3()
+      .then(([buffer, blob]) => {
         var file = new File(buffer, 'voice-recording.mp3', {
           type: blob.type,
-          lastModified: Date.now()
+          lastModified: Date.now(),
         });
-        FileSaver.saveAs(file)       
-      },
-    )}
+        FileSaver.saveAs(file);
+      });
+  }
 
-    return (
-  
-      <div>
-        <button id = "rec"  onClick = {record_start}>Start</button>
-        <button id = "stop" onClick = {record_stop}><p>Stop</p></button>
-      </div>
-    );
+  return (
+    <div>
+      <button id='rec' onClick={record_start}>
+        Start
+      </button>
+      <button id='stop' onClick={record_stop}>
+        <p>Stop</p>
+      </button>
+    </div>
+  );
 }
 
 export default Recorder;
