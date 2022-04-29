cb496c18db9a8fc13804c8d61b61ad7f89585222
MattHeffNT <heffa2004@gmail.com>
2022-04-29 13:39:17 +0930

    minor change to json files and appjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Recorder from './Recorder.js';
-import spiral from  './spiral.png';
+import spiral from './spiral.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={spiral} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={spiral} className='App-logo' alt='logo' />
         <h1> Indigital Voice Recorder</h1>
-        <p>
-          Press the start button when you're ready to start recording
-        </p>
-
+        <p>Press the start button when you're ready to start recording</p>
 
         <br></br>
         <Recorder></Recorder>
-
       </header>
     </div>
   );
