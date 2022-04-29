7f05b1ff3938c4d64672def198c31929e6621b33
MattHeffNT <heffa2004@gmail.com>
2022-04-29 14:18:24 +0930

    change azure config to gh-pages repo

---
+++ build/precache-manifest.a5679f0b248ff21a9a1203513918a78b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f7f920c031c52bf9ac8f59eaa2ebd9",
+    "url": "/voice-recorder/index.html"
+  },
+  {
+    "revision": "b1821c8be3a406b45ab1",
+    "url": "/voice-recorder/static/css/main.2d3bdd2e.chunk.css"
+  },
+  {
+    "revision": "aa1da48258cee58454f1",
+    "url": "/voice-recorder/static/js/2.6d3ae878.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/voice-recorder/static/js/2.6d3ae878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1821c8be3a406b45ab1",
+    "url": "/voice-recorder/static/js/main.d680c43e.chunk.js"
+  },
+  {
+    "revision": "c8fe20e40599171c8772",
+    "url": "/voice-recorder/static/js/runtime-main.0345fda4.js"
+  },
+  {
+    "revision": "65541a20ececc031d21ab0abcfae06bd",
+    "url": "/voice-recorder/static/media/spiral.65541a20.png"
+  }
+]);
